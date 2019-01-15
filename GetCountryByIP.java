98df28aa60609545986ee303f1ed43daca9a54b4
stfedosov <stfedosov@gmail.com>
2019-01-15 17:59:46 +0300

    Update GetCountryByIP.java

--- GetCountryByIP.java
+++ GetCountryByIP.java
@@ -1,17 +1,37 @@
+import com.opencsv.CSVReader;
+import org.apache.hadoop.hive.ql.exec.Description;
+import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.io.Text;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author sfedosov on 12/19/18.
+ */
+@Description(
+        name = "getcountry"
+)
 public class GetCountryByIP extends UDF {
 
     private static final String GEONAME_TO_COUNTRY = "geonametocountry.csv";
     private static final String IP_TO_GEONAME = "iptogeoname.csv";
     private static final String DOT = "\\.";
     private static final String SLASH = "\\/";
-    private static final List<Node> nodesList = new ArrayList<>();
-    private static final char COMMA_SEPARATOR = ',';
-    private static Map<Integer, String> geonameToCountry = new HashMap<>();
+    private static final List<long[]> nodesList = new ArrayList<>(305376);
+    private static Map<Integer, String> geonameToCountry = new HashMap<>(252);
     private Text EMPTY = new Text("empty");
 
     private static void fillMap(Map<Integer, String> map, String source) {
         try (CSVReader reader =
-                     new CSVReader(new InputStreamReader(GetCountryByIP.class.getResourceAsStream(source)), ',')) {
+                     new CSVReader(
+                             new InputStreamReader(
+                                     GetCountryByIP.class.getResourceAsStream(source)))) {
             String[] record;
             while ((record = reader.readNext()) != null) {
                 if (!record[0].isEmpty()) {
@@ -23,61 +43,57 @@ public class GetCountryByIP extends UDF {
         }
     }
 
+    public static void main(String[] args) {
+        readData();
+        fillMap(geonameToCountry, GEONAME_TO_COUNTRY);
+        System.out.println(findGeoName(ipToLong("1.102.213.228".split(DOT))));
+        System.out.println(findGeoName(ipToLong("1.103.222.213".split(DOT))));
+    }
+
     private static void readData() {
         try (CSVReader reader =
-                     new CSVReader(new InputStreamReader(GetCountryByIP.class.getResourceAsStream(IP_TO_GEONAME)), COMMA_SEPARATOR)) {
+                     new CSVReader(
+                             new InputStreamReader(
+                                     GetCountryByIP.class.getResourceAsStream(IP_TO_GEONAME)))) {
             String[] record;
-            int prev = 0;
-            Node prevNode = null;
             while ((record = reader.readNext()) != null) {
                 if (record[1].isEmpty() && record[2].isEmpty()) continue;
                 String[] splitBySlash = record[0].split(SLASH);
-                String[] split = splitBySlash[0].split(DOT);
-                int key = Integer.valueOf(split[0]);
+                long min = ipToLong(splitBySlash[0].split(DOT));
+                long max = min + (1 << 32 - Integer.valueOf(splitBySlash[1]));
                 int geoname = Integer.valueOf(record[1].isEmpty() ? record[2] : record[1]);
-                int netmask = Integer.valueOf(splitBySlash[1]);
-                if (prev != key) {
-                    prev = key;
-                    if (prevNode != null) nodesList.add(prevNode.getChildren().get(0));
-                    prevNode = null;
-                }
-                if (prevNode == null) prevNode = new Node(-1, -1); //fake node
-                prevNode.addChildren(split, 0, netmask, geoname);
-            }
-            if (prevNode != null) {
-                nodesList.add(prevNode.getChildren().get(0));
+                nodesList.add(new long[]{min, max, geoname});
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
+    private static long ipToLong(String[] ipStr) {
+        return Long.valueOf(ipStr[3])
+                | Long.valueOf(ipStr[2]) << 8
+                | Long.valueOf(ipStr[1]) << 16
+                | Long.valueOf(ipStr[0]) << 24;
+    }
+
     public Text evaluate(Text ip) throws SQLException {
-        if (nodesList.isEmpty()) {
-            readData();
-        }
-        if (geonameToCountry.isEmpty()) {
-            fillMap(geonameToCountry, GEONAME_TO_COUNTRY);
-        }
+        if (nodesList.isEmpty()) readData();
+        if (geonameToCountry.isEmpty()) fillMap(geonameToCountry, GEONAME_TO_COUNTRY);
         final String ipStr;
         if (ip == null || (ipStr = ip.toString().trim()).isEmpty()) return EMPTY;
-        final String result = geonameToCountry.get(findGeoName(nodesList, null, ipStr.split(DOT), 0));
+        final String result = geonameToCountry.get(findGeoName(ipToLong(ipStr.split(DOT))));
         return result == null ? EMPTY : new Text(result);
     }
 
-    private static int findGeoName(List<Node> nodesList, Node prev, String[] ip, int index) {
-        if (index == ip.length) return prev.geoname;
-        int current = Integer.valueOf(ip[index]);
+    private static int findGeoName(long toSearch) {
         int start = 0;
         int end = nodesList.size() - 1;
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            Node midNode = nodesList.get(mid);
-            int min = midNode.min;
-            int max = midNode.max;
-            if (min <= current && max >= current) {
-                return findGeoName(midNode.getChildren(), midNode, ip, index + 1);
-            } else if (min > current) {
+            long[] midNode = nodesList.get(mid);
+            if (midNode[0] <= toSearch && midNode[1] >= toSearch) {
+                return (int) midNode[2];
+            } else if (midNode[0] > toSearch) {
                 end = mid - 1;
             } else {
                 start = mid + 1;
