b750bedea72675753f4e012ca63ee7090a6cf16b
stfedosov <stfedosov@gmail.com>
2019-01-16 22:02:38 +0300

    Create QUERIES.sql

---
+++ QUERIES.sql
@@ -0,0 +1,15 @@
+/* top 10 popular categories */
+
+SELECT category, count(*) as t FROM product_purchase GROUP BY category SORT BY t DESC LIMIT 10;
+
+/* top 10 popular products among all categories */
+
+SELECT category, product, freq FROM (
+    SELECT category, product, COUNT(*) AS freq, ROW_NUMBER() OVER (PARTITION BY category ORDER BY COUNT(*) DESC) as seqnum 
+    FROM product_purchase 
+    GROUP BY category, product) 
+ci WHERE seqnum = 1 LIMIT 10;
+
+/*  top 10 countries with the highest money spending */
+
+SELECT getcountry(ip), SUM (price) as s FROM product_purchase GROUP BY getcountry(ip) SORT BY s desc limit 10;
