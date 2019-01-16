108b4ca47146b92f33cf680f66ddbf8da0b83308
stfedosov <stfedosov@gmail.com>
2019-01-16 21:57:17 +0300

    Update TABLES.sql

--- TABLES.sql
+++ TABLES.sql
@@ -1,13 +1,13 @@
-//------------------------
-// product_purchase table:
-//------------------------
+/*-----------------------
+  product_purchase table
+------------------------*/
 
 CREATE EXTERNAL TABLE IF NOT EXISTS 
 product_purchase(product STRING, price DOUBLE, pdate TIMESTAMP, category STRING, ip STRING) 
 PARTITIONED BY (dateval STRING) ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
 WITH SERDEPROPERTIES ( "separatorChar" = "," ) LOCATION '/user/cloudera/flume/events' 
 
-// partitions:
+/* partitions */
 ALTER TABLE product_purchase ADD PARTITION (dateval = '2018-8-1') 
 LOCATION '/user/cloudera/flume/events/2018/8/1';
 ALTER TABLE product_purchase ADD PARTITION (dateval = '2018-8-2') 
@@ -23,9 +23,10 @@ LOCATION '/user/cloudera/flume/events/2018/8/6';
 ALTER TABLE product_purchase ADD PARTITION (dateval = '2018-8-7') 
 LOCATION '/user/cloudera/flume/events/2018/8/7';
 
-//--------------
-// MYSQL tables: 
-//--------------
+/*-------------
+  MYSQL tables 
+--------------*/
+
 CREATE TABLE most_spending_countries (name VARCHAR(50), total DOUBLE);
 CREATE TABLE most_frequent_products (name VARCHAR(50), total INTEGER);
 CREATE TABLE most_frequent_categories (name VARCHAR(50), total INTEGER);
